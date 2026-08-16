967b08f2fec5f90c210d9d10dc9791e11c6c2542
Sameer Marok <sameermarok@gmail.com>
2026-08-15 23:46:32 -0400

    Fixed typo

--- src/App.jsx
+++ src/App.jsx
@@ -85,7 +85,7 @@ const App = () => {
         <header>
           <img src="./hero.png" alt="hero banner"/>
             <h1> Find <span className="text-gradient">Movies</span>
-              You'll Enjoy Without the hastle
+              You'll Enjoy Without the Hassle
             </h1>
 
             <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
