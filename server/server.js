9e4ca82eca5c2176c282a29695e22204b6483e9f
Sameer Marok <sameermarok@gmail.com>
2026-08-21 00:47:36 -0400

    Added Mongoose

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
-const express = require("express");
+import "dotenv/config"; // Load environment variables from .env file
+import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
-// Middleware to parse JSON bodies
 app.use(express.json());
 
 app.get("/", async (req, res) => {
     res.json({ message: "Backend is working!" });
 })
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
-})
+// Connect to MongoDB
+mongoose
+    // Use the connection string from the environment variable
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+
+    app.listen(5000, () => {
+      console.log("Server running on http://localhost:5000");
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection failed:", error);
+  });
